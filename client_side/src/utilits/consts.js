415f4ed4be0c04d2df12053a17f8c93e035fba3b
notPHPuser <artem2005dm@mail.ru>
2024-05-17 14:06:40 +0300

    hz
    hz

--- client_side/src/utilits/consts.js
+++
@@ -1,6 +0,0 @@
-export const MAIN = '/';
-export const APARTMENTS = '/apartments';
-export const LOGIN = '/login';
-export const REGISTER = '/register';
-export const CREATE = '/create';
-export const PROFILE = '/profile';
--- super/src/pages/apartments/ApartmentsPage.js
+++ super/src/pages/apartments/ApartmentsPage.js
@@ -1,189 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../../components/header/Header';
-// import squere from '../../assets/squere.png';
-// import squere_living from '../../assets/squere_living.png';
-// import floor from '../../assets/floor.png';
-// import squere_kitchen from '../../assets/squere_kitchen.png';
-// import bathroom from '../../assets/bathroom.png';
-import { useParams } from 'react-router-dom';
-import { fetchOneApartament } from '../../http/apartamentAPI';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function ApartmentsPage() {
-  const [apartament, setApartment] = useState({ info: [] });
-  const { id } = useParams();
-  console.log(id);
+const ApartmentDetails = ({ id }) => {
+  const [apartment, setApartment] = useState(null);
+
   useEffect(() => {
-    fetchOneApartament(id).then((data) => setApartment(data));
-  });
+    const fetchApartmentData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/apartment/${id}`);
+        setApartment(response.data);
+      } catch (error) {
+        console.error('Error fetching apartment data:', error);
+      }
+    };
 
-  // const apartment = {
-  //   id: 1,
-  //   name: 'pik',
-  //   price: 120000008,
-  //   img: 'https://my-dom.design/wp-content/uploads/2023/07/6.jpg',
-  // };
-  // const description = {
-  //   id: 1,
-  //   squere: 80,
-  //   squere_living: '50',
-  //   squere_kitchen: '20',
-  //   floor: '2',
-  //   bathroom: '2',
-  //   description:
-  //     'Круто вау ахуеть спасибо папаша за тукау красивую иномарку вау капец афигеть вау вау',
-  // };
+    fetchApartmentData();
+  }, [id]);
+
   return (
-    <>
-      <div>
-        <Header />
-        <h1 style={{ position: 'absolute', zIndex: -1, left: 100, top: 70 }}>
-          {apartament.info.name}
-        </h1>
-        <p style={{ position: 'absolute', fontSize: 25, left: 100, top: 120, zIndex: -1 }}>
-          {apartament.info.price} ₽
-        </p>
-        <img
-          style={{ zIndex: -1, position: 'absolute', height: 560, width: 700, left: 100, top: 200 }}
-          src={apartament.info.img}
-        ></img>
-        <button
-          style={{
-            position: 'absolute',
-            backgroundColor: 'inherit',
-            fontSize: 20,
-            display: 'flex',
-            top: 780,
-            left: 100,
-            border: 1,
-            borderStyle: 'solid',
-            borderColor: 'blue',
-            width: 200,
-            height: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 7,
-            cursor: 'pointer',
-          }}
-        >
-          Понравилось
-        </button>
-      </div>
-      {/* <div
-        style={{
-          position: 'relative',
-          top: 110,
-          left: 830,
-          // border: 1,
-          // borderStyle: 'solid',
-          // borderColor: 'red',
-          width: 620,
-          height: 560,
-        }}
-      >
-        <img style={{ position: 'absolute', width: 43, zIndex: -1 }} src={squere}></img>
-        <p
-          style={{
-            color: 'grey',
-            fontSize: 15,
-            position: 'absolute',
-            top: -15,
-            left: 45,
-            zIndex: -1,
-          }}
-        >
-          Общая площадь
-        </p>
-        <p style={{ position: 'absolute', fontSize: 18, zIndex: -1, top: -2, left: 45 }}>
-          {apartament.info.description.squere} м<sup>2</sup>
-        </p>
-        <img
-          style={{ position: 'absolute', width: 43, zIndex: -1, left: 220 }}
-          src={squere_living}
-        ></img>
-        <p style={{ position: 'absolute', fontSize: 18, zIndex: -1, top: -2, left: 270 }}>
-          {apartament.info.description.squere_living} м<sup>2</sup>
-        </p>
-        <p
-          style={{
-            color: 'grey',
-            fontSize: 15,
-            position: 'absolute',
-            top: -15,
-            left: 270,
-            zIndex: -1,
-          }}
-        >
-          Жилая площадь
-        </p>
-        <img style={{ position: 'absolute', width: 43, zIndex: -1, top: 80 }} src={floor}></img>
-        <p
-          style={{
-            position: 'absolute',
-            fontSize: 18,
-            zIndex: -1,
-            top: -2,
-            left: 55,
-            top: 80,
-          }}
-        >
-          {apartament.info.description.floor}
-        </p>
-        <p
-          style={{
-            color: 'grey',
-            fontSize: 15,
-            position: 'absolute',
-            top: -15,
-            left: 55,
-            top: 65,
-            zIndex: -1,
-          }}
-        >
-          Этаж
-        </p>
-        <img
-          style={{ position: 'absolute', width: 43, zIndex: -1, left: 450 }}
-          src={squere_kitchen}
-        ></img>
-        <p style={{ position: 'absolute', fontSize: 18, zIndex: -1, top: -2, left: 510 }}>
-          {apartament.description.squere_kitchen} м<sup>2</sup>
-        </p>
-        <p
-          style={{
-            color: 'grey',
-            fontSize: 15,
-            position: 'absolute',
-            top: -15,
-            left: 510,
-            zIndex: -1,
-          }}
-        >
-          Площадь кухни
-        </p>
-        <img
-          style={{ position: 'absolute', width: 43, zIndex: -1, top: 80, left: 220 }}
-          src={bathroom}
-        ></img>
-        <p style={{ position: 'absolute', fontSize: 18, zIndex: -1, top: -2, left: 270, top: 80 }}>
-          {apartament.info.description.bathroom}
-        </p>
-        <p
-          style={{
-            color: 'grey',
-            fontSize: 15,
-            position: 'absolute',
-            top: 65,
-            left: 270,
-            zIndex: -1,
-          }}
-        >
-          1 или 2
-        </p>
-        <p style={{ position: 'absolute', top: 150 }}>Описание:</p>
-        <p style={{ position: 'absolute', top: 180 }}>{apartament.info.description.description}</p>
-      </div> */}
-    </>
+    <div>
+      {apartment ? (
+        <div>
+          <h2>{apartment.title}</h2>
+          <p>{apartment.description}</p>
+          {/* Render other apartment data fields as needed */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
-}
+};
 
-export default ApartmentsPage;
+export default ApartmentDetails;
